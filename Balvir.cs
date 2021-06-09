3b10ae8bbe4ee666ec615ff1bf3b465d1cfe1aa7
amritpal406 <palsinghamrit560@gmail.com>
2021-06-10 10:50:38 +1200

    Add project files.

---
+++ Balvir.cs
@@ -0,0 +1,12 @@
+﻿namespace RocketRace
+{
+    public class Balvir : Punter
+
+    {
+        public Balvir()
+        {
+            Name = "Balvir";
+            Cash = 50; // he has got 50$
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RocketRace;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGuyNumberBalvir()
+        {
+            Factory.GetNameId("Balvir");
+
+            Assert.AreEqual(0, Factory.GuyNumber);
+        }
+        [TestMethod]
+        public void TestGuyNumberAnil()
+        {
+            Factory.GetNameId("Anil");
+
+            Assert.AreEqual(1, Factory.GuyNumber);
+        }
+        [TestMethod]
+        public void TestGuyNumberAl()
+        {
+            Factory.GetNameId("Al");
+
+            Assert.AreEqual(2, Factory.GuyNumber);
+        }
+    }
+}
